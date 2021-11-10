bc89eb9e487fb3c542b9eeb11f920d6b8442ce47
islandddddd <1207446924@qq.com>
2021-11-10 10:04:39 +0800

    新增二级模态框
    1.新增二级模态框
    2.点击的时候传电机的列表的数据

--- src/views/reserve.vue
+++ src/views/reserve.vue
@@ -6,8 +6,9 @@
 		<router-link :to="{ name: 'officeMap' }">routerlink</router-link>
 		<router-view></router-view>
 	</div> -->
+
 	<!-- 模态框 -->
-	<div class="modal" id="myModal">
+	<div class="modal fade" id="calendar">
 		<div class="modal-dialog modal-dialog-centered modal-dialog-scrollable modal-xl">
 			<div class="modal-content">
 				<!-- 模态框头部 -->
@@ -18,20 +19,7 @@
 
 				<!-- 模态框内容 -->
 				<div class="modal-body">
-					<div class="row">
-						<rcalendar :msg="msg" @childFn="getTime"></rcalendar>
-						<div class="col">
-							<h4>{{ times.date }}</h4>
-							<div class="form-floating">
-								<input type="text" class="form-control" v-model="times.startTime" readonly="readonly" />
-								<label for="floatingInput">开始时间</label>
-							</div>
-							<div class="form-floating">
-								<input type="text" class="form-control" v-model="times.endTime" readonly="readonly" />
-								<label for="floatingInput">结束时间</label>
-							</div>
-						</div>
-					</div>
+					<div><rcalendar :msg="msg" @childFn="getTime"></rcalendar></div>
 				</div>
 
 				<!-- 模态框底部 -->
@@ -39,6 +27,33 @@
 			</div>
 		</div>
 	</div>
+
+	<div class="modal fade" id="times" aria-hidden="true" aria-labelledby="times" tabindex="-1">
+		<div class="modal-dialog modal-dialog-centered">
+			<div class="modal-content">
+				<div class="modal-header">
+					<h5 class="modal-title" id="exampleModalToggleLabel2">Modal 2</h5>
+					<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+				</div>
+				<div class="modal-body">
+					<div>
+						<h4>{{ times.date }}</h4>
+						<div class="form-floating">
+							<input type="text" class="form-control" v-model="times.startTime" readonly="readonly" />
+							<label for="floatingInput">开始时间</label>
+						</div>
+						<div class="form-floating">
+							<input type="text" class="form-control" v-model="times.endTime" readonly="readonly" />
+							<label for="floatingInput">结束时间</label>
+						</div>
+						<button class="btn btn-primary" data-bs-target="#times" data-bs-toggle="modal">Open second modal</button>
+					</div>
+				</div>
+				<div class="modal-footer"><button class="btn btn-primary" data-bs-target="#calendar" data-bs-toggle="modal">Back to first</button></div>
+			</div>
+		</div>
+	</div>
+
 	<div class="container-fluid text-center " style="padding: 0.9375rem;">
 		<div class="row col-6">
 			<div class="col-2 align-self-center">Office</div>
@@ -59,7 +74,7 @@
 			<tr v-for="(item, index) in data">
 				<th>{{ index }}</th>
 				<td v-for="(item, index) in item">{{ item }}</td>
-				<td><button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#myModal" @click="select(index)">select</button></td>
+				<td><button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#calendar" @click="select(index)">select</button></td>
 			</tr>
 		</tbody>
 	</table>
@@ -77,20 +92,51 @@ export default {
 	emits: ['childFn'],
 	data() {
 		return {
-			offliceList: ['office1', 'office2', 'office3'],
-			dataTitle: ['FacilityId', 'FacilityType', 'FacilityPrice', 'select'],
+			offliceList: ['office1', 'office2', 'office3'], //办公室列表
+			dataTitle: ['FacilityId', 'FacilityType', 'FacilityPrice', 'select'], //控制表格的表头
 			data: {
+				//座位数数据
 				'1': {
 					FacilityType: 'FacilityType_1',
-					FacilityPrice: '10$/mo'
+					FacilityPrice: '10$/mo',
+					startTime: 3,
+					endTime: 15,
+					specialHours: {
+						1: dailyHours,
+						2: dailyHours,
+						3: [{ from: 9 * 60, to: 12 * 60, class: 'business-hours' }, { from: 14 * 60, to: 18 * 60, class: 'business-hours' }],
+						4: dailyHours,
+						5: dailyHours
+					},
+					unit: 0
 				},
 				'2': {
 					FacilityType: 'FacilityType_2',
-					FacilityPrice: '20$/mo'
+					FacilityPrice: '20$/mo',
+					startTime: 3,
+					endTime: 15,
+					specialHours: {
+						1: dailyHours,
+						2: dailyHours,
+						3: [{ from: 9 * 60, to: 12 * 60, class: 'business-hours' }, { from: 14 * 60, to: 18 * 60, class: 'business-hours' }],
+						4: dailyHours,
+						5: dailyHours
+					},
+					unit: 1
 				},
 				'3': {
 					FacilityType: 'FacilityType_3',
-					FacilityPrice: '30$/mo'
+					FacilityPrice: '30$/mo',
+					startTime: 3,
+					endTime: 15,
+					specialHours: {
+						1: dailyHours,
+						2: dailyHours,
+						3: [{ from: 9 * 60, to: 12 * 60, class: 'business-hours' }, { from: 14 * 60, to: 18 * 60, class: 'business-hours' }],
+						4: dailyHours,
+						5: dailyHours
+					},
+					unit: 2
 				}
 			},
 			msg: {
@@ -105,12 +151,15 @@ export default {
 				},
 				unit: 2
 			},
-			times: { startTime: '1', endTime: '1' }
+			times: { startTime: '1', endTime: '1' } //日历组件传回的数据
 		};
 	},
 	methods: {
 		select(index) {
+			let dataIndexms = this.data[index];
 			this.msg = this.msg;
+			this.msg = dataIndexms;
+			console.log(dataIndexms);
 		},
 		deleteUser(userid) {
 			alert(userid);
